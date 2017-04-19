afb2d24fd2ded3283cbe818a93ead0b1c12fbf82
yungshenglu <yungshenglu1994@gmail.com>
2017-04-19 17:10:16 +0800

    Create `signin.ejs` and `signin.css`. * Update `signin.ejs` and `signin.css`. * Align `app.js` (not change content).

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// lib
+/* Library */
 const https = require('https');
 const http = require('http');
 const path = require('path');
@@ -6,25 +6,31 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-// core
-const {IntroService} = require('./server-core/intro');
+
+/* Core */
+const { IntroService } = require('./server-core/intro');
+
 /* Redirect views path */
-app.set('views',path.join(__dirname,'client-service/views'));
+app.set('views', path.join(__dirname, 'client-service/views'));
+
 /* Setting static directory - image use */
 app.use(express.static('client-service/elements'));
 app.use(express.static('client-service/images'));
 app.use(express.static('client-service/js'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
-/* Setting view engine as ejs */
-app.set('view engine','ejs');
 
-// Modules
+/* Setting view engine as ejs */
+app.set('view engine', 'ejs');
+
+/* Modules */
 IntroService.init(app);
 
-// Server open
+/* Server start */
 const server = http.createServer(app);
 
-server.listen(process.env.npm_package_config_port,function(){
-    console.log("SCABER server listening on port "+process.env.npm_package_config_port);
+server.listen(process.env.npm_package_config_port, function() {
+    console.log("SCABER server listening on port " + process.env.npm_package_config_port);
 });
