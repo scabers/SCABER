b18645987c19359815df9c735cc0280d7a724fa2
kevinbird61 <kevinbird61@gmail.com>
2017-04-25 22:29:23 +0800

    Add hostname configuration.

--- server-core/auth.js
+++ server-core/auth.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const querystring = require('querystring');
 const GithubStrategy = require('passport-github').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
+const child_process = require('child_process');
 
 // definition here
 class AuthService{
@@ -13,40 +14,72 @@ class AuthService{
         app.use(passport.initialize());
         app.use(passport.session());
         // Facebook Strategy
-        passport.use(new FacebookStrategy({
-            clientID: config.auth.facebook.clientID,
-            clientSecret: config.auth.facebook.clientSecret,
-            profileFields: config.auth.facebook.profileFields,
-            callbackURL: config.auth.facebook.callback
-            },
-            function(accessToken, refreshToken, profile,done){
-                let userdata = profile;
-                return done(null,userdata);
+        child_process.exec("hostname",function(err,stdout,stderr){
+            var hostname = stdout.trim();
+            if(hostname == 'luffy'){
+                // Using luffy configuration
+                passport.use(new FacebookStrategy({
+                    clientID: config.auth.facebook.clientID,
+                    clientSecret: config.auth.facebook.clientSecret,
+                    profileFields: config.auth.facebook.profileFields,
+                    callbackURL: config.auth.facebook.callback_luffy
+                    },
+                    function(accessToken, refreshToken, profile,done){
+                        let userdata = profile;
+                        return done(null,userdata);
+                    }
+                ));
+                // for facebook
+                app.get('/login/facebook',this.fblogin);
+                app.get('/auth/facebook',this.fbauth);
+                app.get('/auth/facebook/callback',passport.authenticate('facebook',{
+                    successReturnToOrRedirect: config.auth.facebook.successUrl_luffy,
+                    failureRedirect: config.auth.facebook.failureUrl_luffy
+                }));
             }
-        ))
+            else{
+                // Using origin configuration
+                passport.use(new FacebookStrategy({
+                    clientID: config.auth.facebook.clientID,
+                    clientSecret: config.auth.facebook.clientSecret,
+                    profileFields: config.auth.facebook.profileFields,
+                    callbackURL: config.auth.facebook.callback
+                    },
+                    function(accessToken, refreshToken, profile,done){
+                        let userdata = profile;
+                        return done(null,userdata);
+                    }
+                ));
+                // for facebook
+                app.get('/login/facebook',this.fblogin);
+                app.get('/auth/facebook',this.fbauth);
+                app.get('/auth/facebook/callback',passport.authenticate('facebook',{
+                    successReturnToOrRedirect: config.auth.facebook.successUrl,
+                    failureRedirect: config.auth.facebook.failureUrl
+                }));
+            }
+            //serialize and deserialize
+            passport.serializeUser(function(user, done) {
+                done(null, user);
+            });
 
-        //serialize and deserialize
-        passport.serializeUser(function(user, done) {
-            done(null, user);
+            passport.deserializeUser(function(user, done) {
+                // And then here: attach user object to req!!
+                done(null, user);
+            });
         });
-
-        passport.deserializeUser(function(user, done) {
-            // And then here: attach user object to req!!
-            done(null, user);
-        });
-
-        // for facebook
-        app.get('/login/facebook',this.fblogin);
-        app.get('/auth/facebook',this.fbauth);
-        app.get('/auth/facebook/callback',passport.authenticate('facebook',{
-            successReturnToOrRedirect: config.auth.facebook.successUrl,
-            failureRedirect: config.auth.facebook.failureUrl
-        }));
     }
 
     fblogin(req,res){
         if(req.isAuthenticated()){
-            res.redirect(config.auth.facebook.successUrl);
+            child_process.exec("hostname",function(err,stdout,stderr){
+                if(stdout.trim() == 'luffy'){
+                    res.redirect(config.auth.facebook.successUrl_luffy);
+                }
+                else{
+                    res.redirect(config.auth.facebook.successUrl);
+                }
+            }
         }
         else{
             // Main Login page ~
@@ -56,7 +89,15 @@ class AuthService{
     fbauth(req,res,next){
         // Using passport to get authenticate
         if (!req.session) req.session = {};
-        req.session.returnTo = config.auth.facebook.successUrl;
+        child_process.exec("hostname",function(err,stdout,stderr){
+            if(stdout.trim() == 'luffy'){
+                req.session.returnTo = config.auth.facebook.successUrl_luffy;
+            }
+            else{
+                req.session.returnTo = config.auth.facebook.successUrl;
+            }
+        }
+
         // Pass them to session
         req.session.email = req.query.email;
         req.session.password = req.query.pwd;
--- server-core/config.js
+++ server-core/config.js
@@ -4,9 +4,12 @@ module.exports = {
             "clientID": "355405361527732",
             "clientSecret": "b0e67a58bce114ff8ebfa3f400ae4bc9",
             "callback": "http://140.116.245.242:3000/auth/facebook/callback",
+            "callback_luffy": "http://luffy.ee.ncku.edu.tw:3000/auth/facebook/callback"
             "profileFields": ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified' , 'picture.type(large)'],
             "successUrl": "http://140.116.245.242:3000/normal",
+            "successUrl_luffy": "http://luffy.ee.ncku.edu.tw:3000/normal",
             "failureUrl": "http://140.116.245.242:3000/error",
+            "failureUrl_luffy": "http://luffy.ee.ncku.edu.tw:3000/error",
             "userAgent": "SCABER"
         }
     }
