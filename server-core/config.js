f8fade468f6d4e2f2e103c1d60a5a36dd45eea16
kevinbird61 <kevinbird61@gmail.com>
2017-04-25 22:35:02 +0800

    Fixed missing comma

--- server-core/config.js
+++ server-core/config.js
@@ -4,7 +4,7 @@ module.exports = {
             "clientID": "355405361527732",
             "clientSecret": "b0e67a58bce114ff8ebfa3f400ae4bc9",
             "callback": "http://140.116.245.242:3000/auth/facebook/callback",
-            "callback_luffy": "http://luffy.ee.ncku.edu.tw:3000/auth/facebook/callback"
+            "callback_luffy": "http://luffy.ee.ncku.edu.tw:3000/auth/facebook/callback",
             "profileFields": ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified' , 'picture.type(large)'],
             "successUrl": "http://140.116.245.242:3000/normal",
             "successUrl_luffy": "http://luffy.ee.ncku.edu.tw:3000/normal",
