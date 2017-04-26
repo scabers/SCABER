f661a9b46c765bed594f6a69ba62ba842da402f6
yungshenglu <yungshenglu1994@gmail.com>
2017-04-26 16:39:23 +0800

    Modified alignment in `app.js` (do not change work).

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 /* Library */
-const https = require('https');
 const http = require('http');
+const https = require('https');
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-// core
-const {IntroService} = require('./server-core/intro');
-const {AuthService} = require('./server-core/auth');
-const {UserService} = require('./server-core/user');
 
-/* Redirect views path */
-app.set('views', path.join(__dirname, 'client-service/views'));
-/* Setting static directory - image use */
+/* Server core */
+const { IntroService } = require('./server-core/intro');
+const { AuthService } = require('./server-core/auth');
+const { UserService } = require('./server-core/user');
+
+const app = express();
+
+/* Setting static directory */
 app.use(express.static('client-service/elements'));
 app.use(express.static('client-service/img'));
 app.use(express.static('client-service/css'));
@@ -23,16 +23,25 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+/* Redirect views path */
+app.set('views', path.join(__dirname, 'client-service/views'));
+
 /* Setting view engine as ejs */
-app.set('view engine','ejs');
-app.use(require('express-session')({ secret: 'SCABER cat', resave: true, saveUninitialized: true }));
+app.set('view engine', 'ejs');
+app.use(require('express-session')({
+    secret: 'SCABER cat',
+    resave: true,
+    saveUninitialized: true
+}));
 
 /* Modules */
 IntroService.init(app);
+
 /* Server start */
 AuthService.init(app);
 UserService.init(app);
-// Server open
+
+/* Create server */
 const server = http.createServer(app);
 
 server.listen(process.env.npm_package_config_port, function() {
