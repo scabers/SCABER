818fd899b28d59cd615100af1ac87f0f89c6db6e
yungshenglu <yungshenglu1994@gmail.com>
2017-05-03 16:58:08 +0800

    Modified the following. * Update `navabar` in the front-end. * Update `index.ejs` for all other `section`

---
+++ client-service/js/theme.js
@@ -0,0 +1,65 @@
+(function($) {
+    // Start of use strict
+    "use strict"
+
+    // Page scrolling feature
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top navbar as scrolling occurs
+    $('body').scrollTop({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+
+    // Close the respomsive menu on menu item click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for navbar
+    $('#navbr').affix({
+        offset: {
+            top: 100
+        }
+    });
+
+    // Initialize and configure scroll reveal animation
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 200);
+    sr.reveal('.sr-button', {
+        duration: 1000,
+        delay: 200
+    });
+    sr.reveal('.sr-contact', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 300);
+
+    // Initialize and configure magnific popup lightbox plugin
+    $('.pop-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigationByImgClick: true,
+            preload: [0, 1]
+        },
+        image: {
+            error: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+})(jQuery);
