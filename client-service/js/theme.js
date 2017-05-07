6a60edd058224a7e93a04bf505bd0a01b9533bdb
yungshenglu <yungshenglu1994@gmail.com>
2017-05-08 01:43:23 +0800

    Modify the following * Add dialogue box into `header` in the `header` of landing page. * Add icon image into circle button in the `header` of landing page. * Modify the class name of `<div>` under `<section>` into `*-section`.

--- client-service/js/theme.js
+++ client-service/js/theme.js
@@ -58,3 +58,64 @@
         }
     });
 })(jQuery);
+
+// Define hover animation in header buttons
+$(document).ready(function() {
+    $('.header-section .header-buttons #rate').hover(function() {
+        $(this).fadeIn(300, function() {
+            $(this).text('');
+            $(this).html("<img src='icon/icon_1.png'>");
+        });
+    }, function() {
+        $(this).fadeIn(300, function() {
+            $(this).text('公開評價');
+        });
+    });
+
+    $('.header-section .header-buttons #rate').click(function() {
+        $('.header-section .header-content .dialogue').toggleClass('dialogue-btn1');
+    });
+
+    $('.header-section .header-buttons #safe').hover(function() {
+        $(this).fadeIn(300, function() {
+            $(this).text('');
+            $(this).html("<img src='icon/icon_3.png'>");
+        });
+    }, function() {
+        $(this).fadeIn(300, function() {
+            $(this).text('安全服務');
+        });
+    });
+
+    $('.header-section .header-buttons #safe').click(function() {
+        $('.header-section .header-content .dialogue').toggleClass('dialogue-btn2');
+    });
+
+    $('.header-section .header-buttons #share').hover(function() {
+        $(this).fadeIn(100, function() {
+            $(this).text('');
+            $(this).html("<img src='icon/icon_2.png'>");
+        });
+    }, function() {
+        $(this).text('共乘模式');
+    });
+
+    $('.header-section .header-buttons #share').click(function() {
+        $('.header-section .header-content .dialogue').toggleClass('dialogue-btn3');
+    });
+
+    $('.header-section .header-buttons #solve').hover(function() {
+        $(this).fadeIn(300, function() {
+            $(this).text('');
+            $(this).html("<img src='icon/icon_5.png'>");
+        });
+    }, function() {
+        $(this).fadeIn(300, function() {
+            $(this).text('解決現況');
+        });
+    });
+
+    $('.header-section .header-buttons #solve').click(function() {
+        $('.header-section .header-content .dialogue').toggleClass('dialogue-btn4');
+    });
+})
