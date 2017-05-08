2dd831c9a51f314a8da1462496593775595d482d
dddd247 <dddd247@gmail.com>
2017-05-09 01:45:47 +0800

    add new way to add animation on signin.js

--- client-service/js/signin.js
+++ client-service/js/signin.js
@@ -1,43 +1,45 @@
 /**這裡是signin.ejs部分的動畫設置 **/
-var  signIn = $('.sign-in'),
-	 signUp = $('.sign-up'),
-	 card1 = $('#signin-left-main-1'),
-	 card2 = $('#signin-left-main-2'),
-	 submit = $('.submit');
-alert("dsdasdas1");
-function ifActive(elem){
-	if (elem.hasClass('activebar')) {
-		return true;
-	} else {
-		return false;
-	}
-}
-alert("dsdasdas2");
+//jQuery time
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
 
-function switchCards(){
-	alert("dsdasdas3");
-	signUp.on('click', function(e){
-		e.preventDefault();
-		if (ifActive(signIn)){
-			signUp.addClass('activebar');
-			signIn.removeClass('activebar');
-			card1.removeClass('signinform').addClass('signupform');
-			card2.removeClass('signupform').addClass('signinform bounceInRight');
-			alert("dsdasdas4");
-		   }
-		}
-	);
-	signIn.on('click', function(e){
-		e.preventDefault();
-		if (ifActive(signUp)) {
-			signUp.removeClass('activebar');
-			signIn.addClass('activebar');
-			card2.removeClass('signinform').addClass('signupform');
-			card1.removeClass('signupform').addClass('signinform animated bounceInLeft');
-		alert("dsdasdas5");
-	    }
-	});
-		 alert("dsdasdas6");
-}
-switchCards();
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
 
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
