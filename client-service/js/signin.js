782f9cad1c99cbf6f80e8b6d2049e592dbbcfc5d
dddd247 <dddd247@gmail.com>
2017-05-09 07:16:25 +0800

    add almost7

--- client-service/js/signin.js
+++ client-service/js/signin.js
@@ -20,8 +20,8 @@ for(var d = 0; d < inputs.length ; d++  ) {
  },100 );
    document.querySelector('.link_forgot_pass').style.opacity = "0";
    document.querySelector('.link_forgot_pass').style.top = "-5px";
-   document.querySelector('.form-check-input').style.opacity = "0";
-   document.querySelector('.form-check-input').style.top = "-5px";
+   document.querySelector('.form-check').style.opacity = "0";
+   document.querySelector('.form-check').style.top = "-5px";
    document.querySelector('.btn_sign').innerHTML = "SIGN UP";    
   setTimeout(function(){
 
@@ -31,7 +31,7 @@ for(var d = 0; d < inputs.length ; d++  ) {
   },500);
   setTimeout(function(){
     document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_none";
-	document.querySelector('.form-check-input').className = "form-check-input d_none";
+	document.querySelector('.form-check').className = "form-check d_none";
  document.querySelector('.terms_and_cons').className = "terms_and_cons d_block";
   },450);
 
@@ -79,7 +79,7 @@ switch(d) {
   setTimeout(function(){
  document.querySelector('.terms_and_cons').className = "terms_and_cons d_none"; 
 document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_block";
-document.querySelector('.form-check-input').className = "form-check-input d_block";
+document.querySelector('.form-check').className = "form-check d_block";
 
  },500);
 
@@ -87,8 +87,8 @@ document.querySelector('.form-check-input').className = "form-check-input d_bloc
 
  document.querySelector('.link_forgot_pass').style.opacity = "1";
    document.querySelector('.link_forgot_pass').style.top = "5px";
-   document.querySelector('.form-check-input').style.opacity = "1";
-   document.querySelector('.form-check-input').style.top = "5px";
+   document.querySelector('.form-check').style.opacity = "1";
+   document.querySelector('.form-check').style.top = "5px";
     
 
 for(var d = 0; d < inputs.length ; d++  ) {
