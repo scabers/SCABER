63cb8e9d544799f92d2ce577ee4e2ea9f65955f3
dddd247 <dddd247@gmail.com>
2017-05-09 16:22:19 +0800

    signin button design6 and fieldset1 design9

--- client-service/js/signin.js
+++ client-service/js/signin.js
@@ -11,7 +11,7 @@ $(".next").click(function(){
 	next_fs = $(this).parent().next();
 	
 	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+	
 	
 	//show the next fieldset
 	next_fs.show(); 
@@ -49,7 +49,7 @@ $(".previous").click(function(){
 	previous_fs = $(this).parent().prev();
 	
 	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+	
 	
 	//show the previous fieldset
 	previous_fs.show(); 
