bbae8b469f7f8977a6643b3490e0a18dbae9b0c4
dddd247 <dddd247@gmail.com>
2017-05-09 09:12:05 +0800

    變成主題模式10 to signup2 fad8 and input1

--- client-service/js/signin.js
+++ client-service/js/signin.js
@@ -56,23 +56,11 @@ switch(d) {
   }
  },100 );
 
- document.querySelector('.terms_and_cons').style.opacity = "0";
-  document.querySelector('.terms_and_cons').style.top = "-5px";
 
-  setTimeout(function(){
- document.querySelector('.terms_and_cons').className = "terms_and_cons d_none"; 
-document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_block";
-document.querySelector('.form-check').className = "form-check d_block";
 
- },500);
 
   setTimeout(function(){
 
- document.querySelector('.link_forgot_pass').style.opacity = "1";
-   document.querySelector('.link_forgot_pass').style.top = "5px";
-   document.querySelector('.form-check').style.opacity = "1";
-   document.querySelector('.form-check').style.top = "5px";
-    
 
 for(var d = 0; d < inputs.length ; d++  ) {
 
@@ -94,8 +82,5 @@ switch(d) {
 }
 
 
-window.onload =function(){
-  document.querySelector('.cont_centrar').className = "cont_centrar cent_active";
 
-}
 
