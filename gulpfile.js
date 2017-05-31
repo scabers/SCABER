6833852b082b4bd62f6a9777a185c906a971e891
kevinbird61 <kevinbird61@gmail.com>
2017-05-31 16:51:23 +0800

    Add watch in gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,18 @@
 var gulp = require('gulp'),             // load gulp module
     gulpSass = require('gulp-sass');    // load gulp-sass module
 
+/* command:
+- gulp <target>: the <target> is the task we want to use, which define in `gulp.task('<target_name>',function(){...})`
+*/
+
+// add "Watch" service
+gulp.task('watch',function(){
+    gulp.watch('scss/**/*.scss',['styles']);        // Assign "styles" work as target
+});
+
+// Work process "styles"
 gulp.task('styles',function(){
     gulp.src('client-service/sass/**/*.scss')       // Get the scss source files
         .pipe(gulpSass())                           // Compile scss
-        .pipe(gulp.dest('client-service/css'));     // destination of css 
+        .pipe(gulp.dest('client-service/css'));     // destination of css
 })
